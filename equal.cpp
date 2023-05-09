eb23e4701ab8eed713147ec4f7f159b951880981
67Ishaan <118922359+67Ishaan@users.noreply.github.com>
2023-05-09 22:54:02 +0530

    Create equal.cpp

---
+++ equal.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int number1, number2;
+ 
+    cout<<"Enter number1 and number2 : ";
+    cin>>number1>>number2;
+    if (number1 == number2)
+    {
+        printf("Number1 and Number2 are equal\n");
+    }    
+    else
+    {
+        printf("Number1 and Number2 are not equal\n");
+    }
+    return 0;
+}
