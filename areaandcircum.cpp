34291c1ac44fd0642401723d517f8cf89dd09110
67Ishaan <118922359+67Ishaan@users.noreply.github.com>
2023-05-09 22:51:23 +0530

    Create areaandcircum.cpp

---
+++ areaandcircum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  float radius, area, circum;
+    cout<<"Enter Radius of Circle : ";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"The area of Circle is : "<<area<<"\n";
+
+    circum=2*3.14*radius;
+    cout<<"The Circumference of Circle is : "<<circum<<"\n";
+    return 0;
+}
