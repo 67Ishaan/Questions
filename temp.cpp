e04237d732d5ca8d76ce6cfb6c9014664e29de25
67Ishaan <118922359+67Ishaan@users.noreply.github.com>
2023-05-09 22:52:06 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float c,f;
+	cout<<"Enter Centigrade : ";
+	cin>>c;
+	f=(9*c)/5+32;
+	
+	cout<<"Fahrenheit : "<<f<<"\n";
+    return 0 ;
+}
