4f7e51acaf7fa93314d668a64610728aaa460b76
67Ishaan <118922359+67Ishaan@users.noreply.github.com>
2023-06-13 12:00:58 +0530

    Create factors.cpp

---
+++ factors.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count=count+1;
+            cout<<i<<"\t";
+        }
+    }
+    cout<<count<<"\n";
+    return 0;
+}
